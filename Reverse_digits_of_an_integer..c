ee9c558ff19d3dd27f28a3df47bc0e6b3fe61aef
Samar <154492013+SupSmr@users.noreply.github.com>
2023-12-23 10:16:05 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int rev=0;     for(;a!=0;a=a/10){         int r=a%10;         rev=rev*10+r;     }     printf("%d",rev); }

---
+++ Reverse_digits_of_an_integer..c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int rev=0;
+    for(;a!=0;a=a/10){
+        int r=a%10;
+        rev=rev*10+r;
+    }
+    printf("%d",rev);
+}
